84e84e63d629263045e45483f6b02bb710bf93de
Aaron Chen <chenjia199242@163.com>
2018-08-02 21:12:43 +0800

    o

---
+++ one/src/main/java/com/racx/XApplication.java
@@ -0,0 +1,12 @@
+package com.racx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XApplication.class, args);
+	}
+}
